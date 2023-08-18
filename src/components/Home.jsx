96cf9e0555f7be8e6ab0ef6407db83752d01c441
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-18 13:05:23 +0530

    initial working of search github profile

---
+++ src/components/Home.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+const Home = () => {
+  const [user, setUser] = useState("none");
+  const [query, setQuery] = useState("webdev-ashishk");
+
+  useEffect(() => {
+    async function getUser() {
+      const res = await fetch(`https://api.github.com/users/${query}`);
+      const data = await res.json();
+      setUser(data);
+    }
+    getUser();
+  }, [query]);
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+  return (
+    <>
+      <div className="container">
+        <div className="input">
+          <input
+            type="text"
+            placeholder="GitHub username"
+            className="inputbox"
+            onChange={handleChange}
+            value={query}
+          />
+        </div>
+        <div className="output">
+          <div className="profile-image">
+            <img src={user.avatar_url} className="circle-image" />
+            <h1 className="login">{user.login}</h1>
+          </div>
+          <div className="otherDetails">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Bio</th>
+                  <th>id</th>
+                  <th>followers</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{user.bio}</td>
+                  <td>{user.id}</td>
+                  <td>{user.followers}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
